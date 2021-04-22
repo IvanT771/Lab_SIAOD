5582aa3ccd61a7448978eac0e49a07a265b79a34
IvanT771 <cnyazev.ivan@yandex.ru>
2021-04-22 14:59:39 +0300

    add task 3

--- Lab4/Stack_Deque/Stack_Deque/Program.cs
+++ Lab4/Stack_Deque/Stack_Deque/Program.cs
@@ -8,8 +8,67 @@ namespace Stack_Deque
 {
     class Program
     {
+        static void S()
+        {
+            Stack stack = new Stack(10);
+
+            stack.Push(5);
+            stack.Push(52);
+
+            Console.WriteLine(stack.Get());
+        }
+        static void D()
+        {
+            Deque deque = new Deque(5);
+            deque.PushStart(1);
+            deque.PushEnd(5);
+            deque.PushEnd(22);
+            deque.PushStart(56);
+            deque.PushStart(31);
+
+            Console.WriteLine(deque.GetEnd());
+            Console.WriteLine(deque.GetStart());
+        }
+
+        //Задача на составление максимального числа
+        static void Task3()
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] numbers = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                numbers[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < N-1; i++)
+            {
+                for (int j = i+1; j < N; j++)
+                {
+                    if(Int64.Parse(numbers[i]+numbers[j]) < Int64.Parse(numbers[j] + numbers[i]))
+                    {
+                        var buf = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j]= buf;
+                    }
+                }
+            }
+
+            string result = "";
+            for (int i = 0; i < N; i++)
+            {
+                result+=numbers[i];
+            }
+
+            Console.WriteLine(Int64.Parse(result));
+
+        }
+
         static void Main(string[] args)
         {
+
+            Task3();
+            Console.Read();
         }
     }
 }
---
+++ Lab4/Stack_Deque/Stack_Deque/Stack.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Deque
+{
+    public class Stack
+    {
+        private int currentIndex=0;
+        private int[] array;
+
+        public Stack(int size)
+        {
+            if (size <= 0) { size = 100;}
+            array = new int[size];
+            currentIndex = -1;
+        }
+
+        //Операция добавления элемента в стэк
+        public void Push(int args)
+        {
+            if (array.Length <= (currentIndex-1)) { Console.WriteLine("Stack full!!!"); return; }
+
+            currentIndex++;
+            array[currentIndex] = args;
+        }
+
+        //Операция удаления последнего элемента
+        public void Delete()
+        {
+            if ((currentIndex-1) < 0) { Console.WriteLine("Stack is empty!"); return; }
+            currentIndex--;
+        }
+
+        //Операция получения элемента
+        public int Get()
+        {
+            if (currentIndex < 0) { Console.WriteLine("Stack is empty!");  return 0;}
+            return array[currentIndex];
+        }
+
+        //Операция проверки на пустоту
+        public bool isEmpty()
+        {
+            if (currentIndex < 0) { return true;}
+            return false;
+        }
+    }
+    public class Deque
+    {
+        private int indexEnd = 0;
+        private int indexStart = 0;
+        private int[] array;
+
+        public Deque(int size)
+        {
+            if (size <= 0) { size = 100; }
+            array = new int[size];
+            indexEnd = -1;
+        }
+
+        //Операция добавления элемента в конец
+        public void PushEnd(int args)
+        {
+            if (array.Length <= (indexEnd)) { Console.WriteLine("Stack full!!!"); return; }
+
+            array[indexEnd] = args;
+            indexEnd++;
+        }
+
+        //Операция добавления элемента в начало
+        public void PushStart(int args)
+        {
+            if (array.Length <= (indexEnd+1)) { Console.WriteLine("Stack full!!!"); return; }
+
+            if (!isEmpty()) { 
+            for (int i = indexEnd; i>=1; i--)
+            {
+                array[i+1] = array[i];
+
+            }
+            }
+            array[0] = args;  
+            indexEnd++;
+        }
+
+        //Операция удаления последнего элемента
+        public void DeleteEnd()
+        {
+            if (isEmpty()) { Console.WriteLine("Stack is empty!"); return; }
+            indexEnd-=2;
+            indexEnd = Math.Max(0,indexEnd);
+        }
+
+        //Операция получения последнего элемента
+        public int GetEnd()
+        {
+            if (indexEnd-1 < 0) { Console.WriteLine("Stack is empty!"); return 0; }
+            return array[indexEnd-1];
+        }
+
+        //Операция получения первого элемента
+        public int GetStart()
+        {
+            if (isEmpty()) { Console.WriteLine("Stack is empty!"); return 0; }
+            return array[0];
+        }
+
+        //Операция проверки на пустоту
+        public bool isEmpty()
+        {
+            if (indexEnd-1 < 0) { return true; }
+            return false;
+        }
+    }
+}
